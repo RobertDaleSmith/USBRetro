2479430e5b910f9c35b1f1a018594adcabc8190b
RobertDaleSmith <robert@robertdalesmith.com>
2023-03-08 01:44:15 -0600

    fix: xinput proper init to correct issues with 8BitDo Pro 2 USB controller

--- PCEMouse/src/xinput_app.c
+++ PCEMouse/src/xinput_app.c
@@ -165,9 +165,9 @@ void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_inter
     return;
   }
   // DISABLED FOR INPUTLABS ALPAKKA - fix on their end and renable later here
-  // tuh_xinput_set_led(dev_addr, instance, 0, true);
-  // tuh_xinput_set_led(dev_addr, instance, 1, true);
-  // tuh_xinput_set_rumble(dev_addr, instance, 0, 0, true);
+  tuh_xinput_set_led(dev_addr, instance, 0, true);
+  tuh_xinput_set_led(dev_addr, instance, 1, true);
+  tuh_xinput_set_rumble(dev_addr, instance, 0, 0, true);
   tuh_xinput_receive_report(dev_addr, instance);
 }
 
