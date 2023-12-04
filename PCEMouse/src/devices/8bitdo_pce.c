fc83ffa2f260b851141e646d2c5c3925ca6efc5f
RobertDaleSmith <robert@robertdalesmith.com>
2023-12-03 23:36:29 -0600

    chore: breaks out 8bitdo_pce hid logic

---
+++ PCEMouse/src/devices/8bitdo_pce.c
@@ -0,0 +1,84 @@
+// 8bitdo_pce.c
+#include "8bitdo_pce.h"
+#include "globals.h"
+
+// check if device is 8BitDo PCE 2.4g controllers
+bool is_8bitdo_pce(uint16_t vid, uint16_t pid)
+{
+  return ((vid == 0x0f0d && pid == 0x0138)); // 8BitDo PCE 2.4g
+}
+
+// check if 2 reports are different enough
+bool diff_report_pce(bitdo_pce_report_t const* rpt1, bitdo_pce_report_t const* rpt2)
+{
+  bool result;
+
+  // x1, y1, x2, y2 must different than 2 to be counted
+  result = diff_than_n(rpt1->x1, rpt2->x1, 2) || diff_than_n(rpt1->y1, rpt2->y1, 2) ||
+           diff_than_n(rpt1->x2, rpt2->x2, 2) || diff_than_n(rpt1->y2, rpt2->y2, 2);
+
+  // Compare the first 3 bytes of the reports
+  result |= memcmp(rpt1, rpt2, 3) != 0;
+
+  return result;
+}
+
+// process usb hid input reports
+void process_8bitdo_pce(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
+  // previous report used to compare for changes
+  static bitdo_pce_report_t prev_report[5] = { 0 };
+
+  bitdo_pce_report_t pce_report;
+  memcpy(&pce_report, report, sizeof(pce_report));
+
+  if (diff_report_pce(&prev_report[dev_addr-1], &pce_report)) {
+    printf("(x1, y1, x2, y2) = (%u, %u, %u, %u)\r\n", pce_report.x1, pce_report.y1, pce_report.x2, pce_report.y2);
+    printf("DPad = %d ", pce_report.dpad);
+
+    if (pce_report.sel) printf("Select ");
+    if (pce_report.run) printf("Run ");
+    if (pce_report.one) printf("I ");
+    if (pce_report.two) printf("II ");
+
+    printf("\r\n");
+
+    bool dpad_up    = (pce_report.dpad == 0 || pce_report.dpad == 1 || pce_report.dpad == 7);
+    bool dpad_right = (pce_report.dpad >= 1 && pce_report.dpad <= 3);
+    bool dpad_down  = (pce_report.dpad >= 3 && pce_report.dpad <= 5);
+    bool dpad_left  = (pce_report.dpad >= 5 && pce_report.dpad <= 7);
+    bool has_6btns = false;
+
+    buttons = (((false)          ? 0x00 : 0x20000) |
+               ((false)          ? 0x00 : 0x10000) |
+               ((false)          ? 0x00 : 0x08000) |
+               ((false)          ? 0x00 : 0x4000) |
+               ((false)          ? 0x00 : 0x2000) |
+               ((false)          ? 0x00 : 0x1000) |
+               ((has_6btns)      ? 0x00 : 0x0800) |
+               ((false)          ? 0x00 : 0x0400) | // home
+               ((false)          ? 0x00 : 0x0200) | // r2
+               ((false)          ? 0x00 : 0x0100) | // l2
+               ((dpad_left)      ? 0x00 : 0x08) |
+               ((dpad_down)      ? 0x00 : 0x04) |
+               ((dpad_right)     ? 0x00 : 0x02) |
+               ((dpad_up)        ? 0x00 : 0x01) |
+               ((pce_report.run) ? 0x00 : 0x80) |
+               ((pce_report.sel) ? 0x00 : 0x40) |
+               ((pce_report.two) ? 0x00 : 0x20) |
+               ((pce_report.one) ? 0x00 : 0x10));
+
+    // add to accumulator and post to the state machine
+    // if a scan from the host machine is ongoing, wait
+    post_globals(dev_addr, instance, buttons, 128, 128, 128, 128, 0, 0, 0, 0);
+
+    prev_report[dev_addr-1] = pce_report;
+  }
+}
+
+DeviceInterface bitdo_pce_interface = {
+  .name = "8BitDo PCE 2.4g",
+  .is_device = is_8bitdo_pce,
+  .process = process_8bitdo_pce,
+  .task = NULL,
+  .init = NULL
+};
--- PCEMouse/src/hid_app.c
+++ PCEMouse/src/hid_app.c
@@ -132,29 +132,6 @@ typedef union
   uint64_t value : 56;
 } pad_buttons;
 
-// 8BitDo USB Adapter for PC Engine 2.4g controllers
-typedef struct TU_ATTR_PACKED
-{
-  struct {
-    uint8_t three : 1;
-    uint8_t two   : 1;
-    uint8_t one   : 1;
-    uint8_t four  : 1;
-  };
-
-  struct {
-    uint8_t sel  : 1;
-    uint8_t run  : 1;
-  };
-
-  struct {
-    uint8_t dpad : 4; // (hat format, 0x08 is released, 0=N, 1=NE, 2=E, 3=SE, 4=S, 5=SW, 6=W, 7=NW)
-  };
-
-  uint8_t x1, y1, x2, y2;
-
-} bitdo_pce_report_t;
-
 // 8BitDo M30 Bluetooth gamepad
 typedef struct TU_ATTR_PACKED
 {
@@ -623,15 +600,6 @@ static inline bool is_dragonrise(uint8_t dev_addr)
   uint16_t pid = devices[dev_addr].pid;
 
   return ((vid == 0x0079 && pid == 0x0011)); // Generic NES USB
-}
-
-// check if device is 8BitDo PCE Controller
-static inline bool is_8bit_pce(uint8_t dev_addr)
-{
-  uint16_t vid = devices[dev_addr].vid;
-  uint16_t pid = devices[dev_addr].pid;
-
-  return ((vid == 0x0f0d && pid == 0x0138)); // 8BitDo PCE (wireless)
 }
 
 // check if device is 8BitDo NeoGeo gamepad
@@ -1229,11 +1197,7 @@ bool isKnownController(uint8_t dev_addr) {
       return true;
     }
   }
-  if (is_8bit_pce(dev_addr)  ) {
-    printf("DEVICE:[8BitDo PCE 2.4g Controller]\n");
-    return true;
-  }
-  else if (is_8bit_neo(dev_addr)  ) {
+  if (is_8bit_neo(dev_addr)  ) {
     printf("DEVICE:[8BitDo NeoGeo 2.4g Controller]\n");
     return true;
   }
@@ -1487,16 +1451,6 @@ void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
   devices[dev_addr].instances[instance].type = CONTROLLER_GENERIC;
 }
 
-bool pce_diff_report(bitdo_pce_report_t const* rpt1, bitdo_pce_report_t const* rpt2)
-{
-  bool result;
-
-  // check the all with mem compare
-  result |= memcmp(&rpt1, &rpt2, 3);
-
-  return result;
-}
-
 bool m30_diff_report(bitdo_m30_report_t const* rpt1, bitdo_m30_report_t const* rpt2)
 {
   bool result;
@@ -1670,60 +1624,6 @@ bool dragonrise_diff_report(dragonrise_report_t const* rpt1, dragonrise_report_t
   result |= memcmp(&rpt1->axis0_y + 1, &rpt2->axis0_y + 1, 2);
 
   return result;
-}
-
-// TODO: possibly deprecatable because of generic HID DirectInput style gamepad report parsing
-void process_8bit_pce(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
-{
-  // previous report used to compare for changes
-  static bitdo_pce_report_t prev_report[5] = { 0 };
-
-  bitdo_pce_report_t pce_report;
-  memcpy(&pce_report, report, sizeof(pce_report));
-
-  if ( pce_diff_report(&prev_report[dev_addr-1], &pce_report) )
-  {
-    printf("(x1, y1, x2, y2) = (%u, %u, %u, %u)\r\n", pce_report.x1, pce_report.y1, pce_report.x2, pce_report.y2);
-    printf("DPad = %d ", pce_report.dpad);
-
-    if (pce_report.sel) printf("Select ");
-    if (pce_report.run) printf("Run ");
-    if (pce_report.one) printf("I ");
-    if (pce_report.two) printf("II ");
-
-    printf("\r\n");
-
-    bool dpad_up    = (pce_report.dpad == 0 || pce_report.dpad == 1 || pce_report.dpad == 7);
-    bool dpad_right = (pce_report.dpad >= 1 && pce_report.dpad <= 3);
-    bool dpad_down  = (pce_report.dpad >= 3 && pce_report.dpad <= 5);
-    bool dpad_left  = (pce_report.dpad >= 5 && pce_report.dpad <= 7);
-    bool has_6btns = false;
-
-    buttons = (((false)          ? 0x00 : 0x20000) |
-               ((false)          ? 0x00 : 0x10000) |
-               ((false)          ? 0x00 : 0x08000) |
-               ((false)          ? 0x00 : 0x4000) |
-               ((false)          ? 0x00 : 0x2000) |
-               ((false)          ? 0x00 : 0x1000) |
-               ((has_6btns)      ? 0x00 : 0x0800) |
-               ((false)          ? 0x00 : 0x0400) | // home
-               ((false)          ? 0x00 : 0x0200) | // r2
-               ((false)          ? 0x00 : 0x0100) | // l2
-               ((dpad_left)      ? 0x00 : 0x08) |
-               ((dpad_down)      ? 0x00 : 0x04) |
-               ((dpad_right)     ? 0x00 : 0x02) |
-               ((dpad_up)        ? 0x00 : 0x01) |
-               ((pce_report.run) ? 0x00 : 0x80) |
-               ((pce_report.sel) ? 0x00 : 0x40) |
-               ((pce_report.two) ? 0x00 : 0x20) |
-               ((pce_report.one) ? 0x00 : 0x10));
-
-    // add to accumulator and post to the state machine
-    // if a scan from the host machine is ongoing, wait
-    post_globals(dev_addr, instance, buttons, 128, 128, 128, 128, 0, 0, 0, 0);
-
-    prev_report[dev_addr-1] = pce_report;
-  }
 }
 
 void process_8bit_m30(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
@@ -2643,8 +2543,7 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
           break;
         }
       }
-      if ( is_8bit_pce(dev_addr) ) process_8bit_pce(dev_addr, instance, report, len);
-      else if ( is_8bit_m30(dev_addr) ) process_8bit_m30(dev_addr, instance, report, len);
+      if ( is_8bit_m30(dev_addr) ) process_8bit_m30(dev_addr, instance, report, len);
       else if ( is_8bit_bta(dev_addr) ) process_8bit_bta(dev_addr, instance, report, len);
       else if ( is_horipad(dev_addr) ) process_horipad(dev_addr, instance, report, len);
       else if ( is_astro_city(dev_addr) ) process_astro_city(dev_addr, instance, report, len);
