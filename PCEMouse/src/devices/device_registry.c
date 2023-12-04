ec448ce14f33c9716905f944b283bae66b218ab8
RobertDaleSmith <robert@robertdalesmith.com>
2023-12-03 23:36:29 -0600

    chore: breaks out 8bitdo_pce hid logic

--- PCEMouse/src/devices/device_registry.c
+++ PCEMouse/src/devices/device_registry.c
@@ -4,6 +4,7 @@
 #include "sony_ds4.h"
 #include "sony_ds5.h"
 #include "sony_psc.h"
+#include "8bitdo_pce.h"
 // Include other devices here
 
 DeviceInterface* device_interfaces[MAX_DEVICE_TYPES] = {0};
@@ -13,6 +14,7 @@ void register_devices() {
     device_interfaces[1] = &sony_ds4_interface;
     device_interfaces[2] = &sony_ds5_interface;
     device_interfaces[3] = &sony_psc_interface;
+    device_interfaces[4] = &bitdo_pce_interface;
     // Register other devices here
     // device_interfaces[1] = &another_device_interface;
 }
