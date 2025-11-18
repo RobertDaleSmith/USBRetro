51787fdef7614ec34bf63b30f5e217dc52fce8ce
Robert Dale Smith <robert@robertdalesmith.com>
2025-11-18 01:53:27 -0600

    Add Unix Makefiles generator to build scripts for Windows compatibility
    Explicitly specify CMake generator as 'Unix Makefiles' in all build scripts.
    This enables Windows users with Git Bash + ARM GCC + Make toolchain to
    build without CMake defaulting to Visual Studio generators.
    
    No impact on macOS/Linux where Unix Makefiles is already the default.

--- src/build_rpi_pico_debug.sh
+++ src/build_rpi_pico_debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake -DFAMILY=rp2040 -DBOARD=raspberry_pi_pico -DCMAKE_BUILD_TYPE=Debug -B build
+cmake -G "Unix Makefiles" -DFAMILY=rp2040 -DBOARD=raspberry_pi_pico -DCMAKE_BUILD_TYPE=Debug -B build
--- src/build_waveshare_rp2040_zero.sh
+++ src/build_waveshare_rp2040_zero.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake -DFAMILY=rp2040 -DPICO_BOARD=waveshare_rp2040_zero -B build
+cmake -G "Unix Makefiles" -DFAMILY=rp2040 -DPICO_BOARD=waveshare_rp2040_zero -B build
