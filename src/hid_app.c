a99416968a235e7a8935e49f216f17bcc58a6c6d
Robert Dale Smith <robert@robertdalesmith.com>
2025-11-21 23:58:59 -0600

    Rename devices/ to usbh/ for USB Host driver clarity
    Renamed src/devices/ to src/usbh/ to accurately reflect that this is the
    USB Host driver stack, not USB Device mode.
    
    Benefits:
    - Technically precise terminology (USB Host mode)
    - Clearer purpose for standalone library
    - Follows industry standard naming
    - Better encapsulation of USB input layer
    
    Updated all include paths and CMakeLists.txt source references.

--- src/hid_app.c
+++ src/hid_app.c
@@ -1,8 +1,8 @@
 // hid_app.c
 #include "tusb.h"
 #include "globals.h"
-#include "devices/device_utils.h"
-#include "devices/device_registry.h"
+#include "usbh/device_utils.h"
+#include "usbh/device_registry.h"
 #include "common/profile_indicator.h"
 
 // #define LANGUAGE_ID 0x0409
