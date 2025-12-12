08db13260ec894e5bb736e8ebebd1e183b877ac1
Robert Dale Smith <robert@robertdalesmith.com>
2025-12-12 01:48:32 -0600

    Reorder button values to W3C Gamepad API standard
    Bit position now equals W3C button index (1 << index)
    Sequential bits 0-20 for all buttons
    Cleaner and matches web standard

--- src/core/buttons.h
+++ src/core/buttons.h
@@ -1,33 +1,46 @@
 // buttons.h
-// USBRetro canonical button definitions (GP2040-CE compatible)
+// USBRetro canonical button definitions (W3C Gamepad API order)
 #ifndef BUTTONS_H
 #define BUTTONS_H
 
 #define MAX_DEVICES 6
 
-// USBRetro Buttons -> Xinput/Switch/PlayStation
-#define USBR_BUTTON_B1 0x00020 // A/B/Cross
-#define USBR_BUTTON_B2 0x00010 // B/A/Circle
-#define USBR_BUTTON_B3 0x02000 // X/Y/Square
-#define USBR_BUTTON_B4 0x01000 // Y/X/Triangle
-#define USBR_BUTTON_L1 0x04000 // LB/L/L1
-#define USBR_BUTTON_R1 0x08000 // RB/R/R1
-#define USBR_BUTTON_L2 0x00100 // LT/ZL/L2
-#define USBR_BUTTON_R2 0x00200 // RT/ZR/R2
-#define USBR_BUTTON_S1 0x00040 // Back/Minus/Share/Select
-#define USBR_BUTTON_S2 0x00080 // Start/Plus/Options
-#define USBR_BUTTON_L3 0x10000 // LS/L3
-#define USBR_BUTTON_R3 0x20000 // RS/R3
-#define USBR_BUTTON_A1 0x00400  // Guide/Home/PS
-#define USBR_BUTTON_A2 0x00800  // Capture/Touchpad
-#define USBR_BUTTON_A3 0x100000 // Mute (DS5)
-#define USBR_BUTTON_L4 0x40000  // Rear Left/Paddle/Touchpad Left
-#define USBR_BUTTON_R4 0x80000  // Rear Right/Paddle/Touchpad Right
+// W3C Gamepad API standard button order
+// Bit position = W3C button index (trivial conversion: 1 << index)
 
-// USBRetro Dpad Directions
-#define USBR_BUTTON_DU 0x00001 // Dpad Up
-#define USBR_BUTTON_DD 0x00004 // Dpad Down
-#define USBR_BUTTON_DL 0x00008 // Dpad Left
-#define USBR_BUTTON_DR 0x00002 // Dpad Right
+// Face buttons (right cluster)
+#define USBR_BUTTON_B1 (1 << 0)   // 0 - Bottom (Cross/A)
+#define USBR_BUTTON_B2 (1 << 1)   // 1 - Right (Circle/B)
+#define USBR_BUTTON_B3 (1 << 2)   // 2 - Left (Square/X)
+#define USBR_BUTTON_B4 (1 << 3)   // 3 - Top (Triangle/Y)
+
+// Shoulder buttons (front)
+#define USBR_BUTTON_L1 (1 << 4)   // 4 - Top left front (L1/LB)
+#define USBR_BUTTON_R1 (1 << 5)   // 5 - Top right front (R1/RB)
+#define USBR_BUTTON_L2 (1 << 6)   // 6 - Bottom left front (L2/LT)
+#define USBR_BUTTON_R2 (1 << 7)   // 7 - Bottom right front (R2/RT)
+
+// Center cluster
+#define USBR_BUTTON_S1 (1 << 8)   // 8 - Left center (Select/Back/Share)
+#define USBR_BUTTON_S2 (1 << 9)   // 9 - Right center (Start/Options)
+
+// Stick clicks
+#define USBR_BUTTON_L3 (1 << 10)  // 10 - Left stick click
+#define USBR_BUTTON_R3 (1 << 11)  // 11 - Right stick click
+
+// D-pad (left cluster)
+#define USBR_BUTTON_DU (1 << 12)  // 12 - D-pad Up
+#define USBR_BUTTON_DD (1 << 13)  // 13 - D-pad Down
+#define USBR_BUTTON_DL (1 << 14)  // 14 - D-pad Left
+#define USBR_BUTTON_DR (1 << 15)  // 15 - D-pad Right
+
+// Auxiliary (center)
+#define USBR_BUTTON_A1 (1 << 16)  // 16 - Center (Guide/Home/PS)
+
+// Extended buttons (beyond W3C standard)
+#define USBR_BUTTON_A2 (1 << 17)  // 17 - Touchpad/Capture
+#define USBR_BUTTON_A3 (1 << 18)  // 18 - Mute (DualSense)
+#define USBR_BUTTON_L4 (1 << 19)  // 19 - Rear left paddle
+#define USBR_BUTTON_R4 (1 << 20)  // 20 - Rear right paddle
 
 #endif // BUTTONS_H
